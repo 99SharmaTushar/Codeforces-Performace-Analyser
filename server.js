4a21bff5303b53d8bde14f2201586401f56a7881
jackie <99sharmatushar@gmail.com>
2021-05-07 16:49:21 +0530

    version 1.0.0

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 
-const userRouter = require('./routes/user.js');
-const contestRouter = require('./routes/contest.js');
+const userRouter = require("./routes/user.js");
+const contestRouter = require("./routes/contest.js");
 
-app.use('/user',userRouter);
-app.use('/contest',contestRouter);
+app.use("/user", userRouter);
+app.use("/contest", contestRouter);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("Frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
-
